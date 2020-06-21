6032348698aa708a4440fdc2c156ab12740babbc
Gaurav Bharti <gaurav.bharti1998@gmail.com>
2020-06-21 19:57:26 +0530

    added link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { grey } from 'color-name';
 
 class Footer extends React.Component{
     constructor(){
@@ -13,9 +14,15 @@ class Footer extends React.Component{
         this.setState({time: new Date()})
     }
     render(){
+        const style1={
+            fontSize: '25px',
+            textAlign : 'right',
+            color: 'grey'
+        }
         return(
             <div>
                 <h3>{this.state.time.getHours().toString().padStart(2,'0')}:{this.state.time.getMinutes().toString().padStart(2,'0')}:{this.state.time.getSeconds().toString().padStart(2,'0')}</h3>
+                <h5 style ={style1}><a href ="https://github.com/Gauravbharti1998/ReactPractice">Click to see GitHub Repo</a></h5>
                 
             </div>
         ) 
